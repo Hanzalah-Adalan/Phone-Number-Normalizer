0e99187c1284ec02cbb5481c25beb464f933f07b
Hanzalah Adalan <hanzalahstayzero@outlook.com>
2020-11-30 08:37:41 +0800

    last from office PC

--- Phone Number Normalizer/MainWindow.xaml.cs
+++ Phone Number Normalizer/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using OfficeOpenXml;
 using OfficeOpenXml.Table;
+using System.Diagnostics;
 
 namespace Phone_Number_Normalizer
 {
@@ -51,8 +52,86 @@ namespace Phone_Number_Normalizer
             DependencyProperty.Register("NumberWithHyphenCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
 
 
+
+        public int NumberWithWhitespaceCount
+        {
+            get { return (int)GetValue(NumberWithWhitespaceCountProperty); }
+            set { SetValue(NumberWithWhitespaceCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NumberWithWhitespaceCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NumberWithWhitespaceCountProperty =
+            DependencyProperty.Register("NumberWithWhitespaceCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+        public int CountryCodeCounter
+        {
+            get { return (int)GetValue(CountryCodeCounterProperty); }
+            set { SetValue(CountryCodeCounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CountryCodeCounter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CountryCodeCounterProperty =
+            DependencyProperty.Register("CountryCodeCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+        public int OtherCountryCodeCounter
+        {
+            get { return (int)GetValue(OtherCountryCodeCounterProperty); }
+            set { SetValue(OtherCountryCodeCounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OtherCountryCodeCounter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OtherCountryCodeCounterProperty =
+            DependencyProperty.Register("OtherCountryCodeCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public int MultipleNumberCounter
+        {
+            get { return (int)GetValue(MultipleNumberCounterProperty); }
+            set { SetValue(MultipleNumberCounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MultipleNumberCounter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MultipleNumberCounterProperty =
+            DependencyProperty.Register("MultipleNumberCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public int NumberWithAlphabetCounter
+        {
+            get { return (int)GetValue(NumberWithAlphabetCounterProperty); }
+            set { SetValue(NumberWithAlphabetCounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NumberWithAlphabetCounter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NumberWithAlphabetCounterProperty =
+            DependencyProperty.Register("NumberWithAlphabetCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
+        public int LandlineCounter
+        {
+            get { return (int)GetValue(LandlineCounterProperty); }
+            set { SetValue(LandlineCounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LandlineCounter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LandlineCounterProperty =
+            DependencyProperty.Register("LandlineCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            listbox.ItemsSource = null;
             try
             {
                 var _columnLetterPosition = GetColumnName(dataColumn.Position);
@@ -65,61 +144,58 @@ namespace Phone_Number_Normalizer
 
                 foreach (var cell in _cellRange)
                 {
-                    string _holder = "";
-                    string _midHolder = "";
+                    //string _holder = "";
+                    //string _midHolder = "";
 
                     var _sourceString = cell.Value.ToString();
-                    var _sb = Regex.Replace(_sourceString, @"\s+", "");
+                    
+                    if (Regex.IsMatch(_sourceString, @"\s+"))
+                    {
+                        NumberWithWhitespaceCount++;
 
-                    if (_sb.StartsWith("(+60)"))
-                    {
-                        _holder = _sb.Replace("(+60)", "0");
-                    }
-                    else if (_sb.StartsWith("+60"))
-                    {
-                        _holder = _sb.Replace("+60", "0");
-                    }
-                    else
-                    {
-                        _holder = _sb;
+                        if(chkBox_removeWhiteSpace.IsChecked == true)
+                            _sourceString = Regex.Replace(_sourceString, @"\s+", "");
                     }
 
-                    if (chkBox_removeHyphen.IsChecked == true && Regex.IsMatch(_holder, @"\b.+-.+"))
-                    {                        
-                        _midHolder = Regex.Replace(_holder, @"-", "");
+                    if (Regex.IsMatch(_sourceString, @"@/&"))
+                    {
+                        MultipleNumberCounter++;
                     }
-                    else
+
+                    if (Regex.IsMatch(_sourceString, @""))
+                    {
+
+                    }
+
+                    if (_sourceString.StartsWith("(+60)"))
+                    {
+                        CountryCodeCounter++;
+                        _sourceString = _sourceString.Replace("(+60)", "0");
+                    }
+                    else if (_sourceString.StartsWith("+60"))
+                    {
+                        CountryCodeCounter++;
+                        _sourceString = _sourceString.Replace("+60", "0");
+                    }
+
+                    if (Regex.IsMatch(_sourceString, @"\b.+-.+"))
                     {
                         NumberWithHyphenCount++;
-                        _midHolder = _holder;
+                        if (chkBox_removeHyphen.IsChecked == true)
+                        {
+                            _sourceString = Regex.Replace(_sourceString, @"-", "");
+                        }                       
                     }
 
-                    numbers.Add(_midHolder);
+                    numbers.Add(_sourceString);
                 }
 
                 listbox.ItemsSource = numbers;
+                IsExportButtonEnabled = true;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void btnTestSingleBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrWhiteSpace(txtBox.Text))
-            {
-                if (Regex.IsMatch(txtBox.Text, @"\b.+-.+"))
-                {
-                    txtBox.Text = Regex.Replace(txtBox.Text, @"-", "");
-
-                    MessageBox.Show("Successfully removed");
-                }
-                else
-                {
-                    MessageBox.Show("Failed to remove");
-                }
-
             }
         }
 
@@ -128,8 +204,11 @@ namespace Phone_Number_Normalizer
 
         private async void btn_loadFile_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openfileDialog1 = new OpenFileDialog
+            {
+                Filter = "Excel Files|*.xls;*.xlsx;*.xlsm"
+            };
 
-            OpenFileDialog openfileDialog1 = new OpenFileDialog();
             if (openfileDialog1.ShowDialog() == true)
             {
                 FileInfo file = new FileInfo(openfileDialog1.FileName);
@@ -140,7 +219,7 @@ namespace Phone_Number_Normalizer
                 await package.LoadAsync(file);
                 workbook = package.Workbook;
 
-                txtBlock_filename.Text = openfileDialog1.SafeFileName;
+                txtBlock_filename.Content = openfileDialog1.SafeFileName;
 
                 foreach (var item in package.Workbook.Worksheets)
                 {
@@ -158,17 +237,20 @@ namespace Phone_Number_Normalizer
             {
                 sheet = _ws;
 
-                var _columns = _ws.Tables.FirstOrDefault().Columns;
-                foreach (var item in _columns)
+                if (_ws.Tables.Count > 0)
                 {
-                    cmbBox_columnSelector.Items.Add(item);
+                    var _columns = _ws.Tables.FirstOrDefault().Columns;
+                    foreach (var item in _columns)
+                    {
+                        cmbBox_columnSelector.Items.Add(item);
+                    }
+                    IsColumnSelectorEnabled = true;
                 }
-
-                IsColumnSelectorEnabled = true;
-            }
-            else
-            {
-                IsColumnSelectorEnabled = false;
+                else
+                {
+                    IsColumnSelectorEnabled = false;
+                    MessageBox.Show("No table(s) detected in this sheet. please create a table by pressing Ctrl + T to proceed");
+                }                
             }
         }
 
@@ -215,6 +297,18 @@ namespace Phone_Number_Normalizer
 
 
 
+        public bool IsExportButtonEnabled
+        {
+            get { return (bool)GetValue(IsExportButtonEnabledProperty); }
+            set { SetValue(IsExportButtonEnabledProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsExportButtonEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsExportButtonEnabledProperty =
+            DependencyProperty.Register("IsExportButtonEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+
+
+
         public bool IsColumnSelectorEnabled
         {
             get { return (bool)GetValue(IsColumnSelectorEnabledProperty); }
@@ -237,6 +331,63 @@ namespace Phone_Number_Normalizer
         public static readonly DependencyProperty IsManipulationButtonsEnabledProperty =
             DependencyProperty.Register("IsManipulationButtonsEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                string _sourceString = Clipboard.GetText(TextDataFormat.Text);
+                // Do whatever you need to do with clipboardText
+
+                if (!string.IsNullOrEmpty(_sourceString))
+                {
+                    try
+                    {
+                        if (Regex.IsMatch(_sourceString, @"\s+"))
+                        {
+                            _sourceString = Regex.Replace(_sourceString, @"\s+", "");                               
+                        }
+
+
+                        if (_sourceString.StartsWith("(+60)"))
+                        {
+                            _sourceString = _sourceString.Replace("(+60)", "0");
+                        }
+                        else if (_sourceString.StartsWith("+60"))
+                        {
+                            _sourceString = _sourceString.Replace("+60", "0");
+                        }
+
+                        if (Regex.IsMatch(_sourceString, @"\b.+-.+"))
+                        {
+                            _sourceString = Regex.Replace(_sourceString, @"-", "");
+                        }
+
+                        if (!_sourceString.StartsWith("6"))
+                        {
+                            _sourceString = $"6{_sourceString}";
+                        }
+
+                        if (chkBox_useNativeWhatsapp.IsChecked == true)
+                        {
+                            Process.Start(new ProcessStartInfo($"whatsapp://send?phone={_sourceString}&text=message") { UseShellExecute = true });
+                        }
+                        else
+                        {
+                            Process.Start(new ProcessStartInfo($"https://api.whatsapp.com/send?phone={_sourceString}&text=message") { UseShellExecute = true });
+                        }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
     }
 }
