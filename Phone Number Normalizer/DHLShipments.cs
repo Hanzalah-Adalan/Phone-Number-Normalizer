07cd9aeaa98b5ace1523f5bb1ea2b8d256d8434e
Hanzalah Adalan <hanzalahstayzero@outlook.com>
2020-12-01 17:58:03 +0800

    Added DHL tracking support

---
+++ Phone Number Normalizer/DHLShipments.cs
@@ -0,0 +1,121 @@
+﻿namespace DHL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class DhlShipments
+    {
+        [JsonProperty("shipments")]
+        public List<Shipment> Shipments { get; set; }
+
+        [JsonProperty("possibleAdditionalShipmentsUrl")]
+        public List<string> PossibleAdditionalShipmentsUrl { get; set; }
+    }
+
+    public partial class Shipment
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("service")]
+        public string Service { get; set; }
+
+        [JsonProperty("origin")]
+        public Destination Origin { get; set; }
+
+        [JsonProperty("destination")]
+        public Destination Destination { get; set; }
+
+        [JsonProperty("status")]
+        public Status Status { get; set; }
+
+        [JsonProperty("details")]
+        public Details Details { get; set; }
+
+        [JsonProperty("events")]
+        public List<Status> Events { get; set; }
+    }
+
+    public partial class Destination
+    {
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+    }
+
+    public partial class Address
+    {
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+
+        [JsonProperty("postalCode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("addressLocality")]
+        public string AddressLocality { get; set; }
+    }
+
+    public partial class Details
+    {
+        [JsonProperty("product")]
+        public Product Product { get; set; }
+
+        [JsonProperty("references")]
+        public List<Reference> References { get; set; }
+    }
+
+    public partial class Product
+    {
+        [JsonProperty("productName")]
+        public string ProductName { get; set; }
+    }
+
+    public partial class Reference
+    {
+        [JsonProperty("number")]
+        public string Number { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public partial class Status
+    {
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("location")]
+        public Destination Location { get; set; }
+
+        [JsonProperty("statusCode")]
+        public string StatusCode { get; set; }
+
+        [JsonProperty("status")]
+        public string StatusStatus { get; set; }
+    }
+
+    public partial class DhlShipments
+    {
+        public static DhlShipments FromJson(string json) => JsonConvert.DeserializeObject<DhlShipments>(json, DHL.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this DhlShipments self) => JsonConvert.SerializeObject(self, DHL.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
--- Phone Number Normalizer/MainWindow.xaml.cs
+++ Phone Number Normalizer/MainWindow.xaml.cs
@@ -17,13 +17,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
-using OfficeOpenXml;
-using OfficeOpenXml.Table;
 using System.Net.Http;
 using System.Diagnostics;
 using HtmlAgilityPack;
 using RestSharp;
 using MaterialDesignExtensions.Model;
+using System.Net;
 
 namespace Phone_Number_Normalizer
 {
@@ -42,6 +41,12 @@ namespace Phone_Number_Normalizer
             _listener.OnKeyPressed += _listener_OnKeyPressed;
 
             _listener.HookKeyboard();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtBox_cookie.Text = Properties.Settings.Default.HCookie;
         }
 
         public Visibility IsFetchingUpline
@@ -69,7 +74,7 @@ namespace Phone_Number_Normalizer
 
         void _listener_OnKeyPressed(object sender, KeyPressedArgs e)
         {
-            if (Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftShift) && e.KeyPressed == Key.U)
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftShift) && e.KeyPressed == Key.O)
             {
                 if (this.WindowState == WindowState.Minimized)
                     this.WindowState = WindowState.Normal;
@@ -91,16 +96,6 @@ namespace Phone_Number_Normalizer
 
                             customerID.Text = _sourceString;
                             btn_findUpline_Click(null, null);
-
-                            //if (chkBox_useNativeWhatsapp.IsChecked == true)
-                            //{
-                            //    Process.Start(new ProcessStartInfo($"whatsapp://send?phone={_sourceString}&text=message") { UseShellExecute = true });
-                            //}
-                            //else
-                            //{
-                            //    Process.Start(new ProcessStartInfo($"https://api.whatsapp.com/send?phone={_sourceString}&text=message") { UseShellExecute = true });
-                            //}
-
                         }
                         catch (Exception ex)
                         {
@@ -116,300 +111,6 @@ namespace Phone_Number_Normalizer
             _listener.UnHookKeyboard();
         }
 
-        List<string> numbers = new List<string>();
-        ExcelWorkbook workbook;
-        ExcelWorksheet sheet;
-
-
-
-        public int NumberWithHyphenCount
-        {
-            get { return (int)GetValue(NumberWithHyphenCountProperty); }
-            set { SetValue(NumberWithHyphenCountProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for NumberWithHyphenCount.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty NumberWithHyphenCountProperty =
-            DependencyProperty.Register("NumberWithHyphenCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-        public int NumberWithWhitespaceCount
-        {
-            get { return (int)GetValue(NumberWithWhitespaceCountProperty); }
-            set { SetValue(NumberWithWhitespaceCountProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for NumberWithWhitespaceCount.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty NumberWithWhitespaceCountProperty =
-            DependencyProperty.Register("NumberWithWhitespaceCount", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-        public int CountryCodeCounter
-        {
-            get { return (int)GetValue(CountryCodeCounterProperty); }
-            set { SetValue(CountryCodeCounterProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for CountryCodeCounter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty CountryCodeCounterProperty =
-            DependencyProperty.Register("CountryCodeCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-        public int OtherCountryCodeCounter
-        {
-            get { return (int)GetValue(OtherCountryCodeCounterProperty); }
-            set { SetValue(OtherCountryCodeCounterProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for OtherCountryCodeCounter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty OtherCountryCodeCounterProperty =
-            DependencyProperty.Register("OtherCountryCodeCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-
-        public int MultipleNumberCounter
-        {
-            get { return (int)GetValue(MultipleNumberCounterProperty); }
-            set { SetValue(MultipleNumberCounterProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for MultipleNumberCounter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MultipleNumberCounterProperty =
-            DependencyProperty.Register("MultipleNumberCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-
-        public int NumberWithAlphabetCounter
-        {
-            get { return (int)GetValue(NumberWithAlphabetCounterProperty); }
-            set { SetValue(NumberWithAlphabetCounterProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for NumberWithAlphabetCounter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty NumberWithAlphabetCounterProperty =
-            DependencyProperty.Register("NumberWithAlphabetCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-
-        public int LandlineCounter
-        {
-            get { return (int)GetValue(LandlineCounterProperty); }
-            set { SetValue(LandlineCounterProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for LandlineCounter.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty LandlineCounterProperty =
-            DependencyProperty.Register("LandlineCounter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-
-
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            listbox.ItemsSource = null;
-            try
-            {
-                var _columnLetterPosition = GetColumnName(dataColumn.Position);
-
-                var _address = $"{_columnLetterPosition}:{_columnLetterPosition}";
-
-                var _cellRange = sheet.Cells[_address];
-
-                //System.Diagnostics.Debug.WriteLine($"Total row count: {_phoneNumberColumn.Count()}");
-
-                foreach (var cell in _cellRange)
-                {
-                    //string _holder = "";
-                    //string _midHolder = "";
-
-                    var _sourceString = cell.Value.ToString();
-                    
-                    if (Regex.IsMatch(_sourceString, @"\s+"))
-                    {
-                        NumberWithWhitespaceCount++;
-
-                        if(chkBox_removeWhiteSpace.IsChecked == true)
-                            _sourceString = Regex.Replace(_sourceString, @"\s+", "");
-                    }
-
-                    if (Regex.IsMatch(_sourceString, @"@/&"))
-                    {
-                        MultipleNumberCounter++;
-                    }
-
-                    if (Regex.IsMatch(_sourceString, @""))
-                    {
-
-                    }
-
-                    if (_sourceString.StartsWith("(+60)"))
-                    {
-                        CountryCodeCounter++;
-                        _sourceString = _sourceString.Replace("(+60)", "0");
-                    }
-                    else if (_sourceString.StartsWith("+60"))
-                    {
-                        CountryCodeCounter++;
-                        _sourceString = _sourceString.Replace("+60", "0");
-                    }
-
-                    if (Regex.IsMatch(_sourceString, @"\b.+-.+"))
-                    {
-                        NumberWithHyphenCount++;
-                        if (chkBox_removeHyphen.IsChecked == true)
-                        {
-                            _sourceString = Regex.Replace(_sourceString, @"-", "");
-                        }                       
-                    }
-
-                    numbers.Add(_sourceString);
-                }
-
-                listbox.ItemsSource = numbers;
-                IsExportButtonEnabled = true;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        
-        ExcelPackage package;
-
-        private async void btn_loadFile_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openfileDialog1 = new OpenFileDialog
-            {
-                Filter = "Excel Files|*.xls;*.xlsx;*.xlsm"
-            };
-
-            if (openfileDialog1.ShowDialog() == true)
-            {
-                FileInfo file = new FileInfo(openfileDialog1.FileName);
-
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                package = new ExcelPackage();
-
-                await package.LoadAsync(file);
-                workbook = package.Workbook;
-
-                txtBlock_filename.Content = openfileDialog1.SafeFileName;
-
-                foreach (var item in package.Workbook.Worksheets)
-                {
-                    cmbBox_sheetSelector.Items.Add(item);
-                }
-            }
-
-            IsSheetSelectorEnabled = !string.IsNullOrEmpty(openfileDialog1.FileName);
-
-        }
-
-        private void cmbBox_sheetSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (cmbBox_sheetSelector.SelectedItem is ExcelWorksheet _ws)
-            {
-                sheet = _ws;
-
-                if (_ws.Tables.Count > 0)
-                {
-                    var _columns = _ws.Tables.FirstOrDefault().Columns;
-                    foreach (var item in _columns)
-                    {
-                        cmbBox_columnSelector.Items.Add(item);
-                    }
-                    IsColumnSelectorEnabled = true;
-                }
-                else
-                {
-                    IsColumnSelectorEnabled = false;
-                    MessageBox.Show("No table(s) detected in this sheet. please create a table by pressing Ctrl + T to proceed");
-                }                
-            }
-        }
-
-        static string GetColumnName(int index)
-        {
-            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            var value = "";
-
-            if (index >= letters.Length)
-                value += letters[index / letters.Length - 1];
-
-            value += letters[index % letters.Length];
-
-            return value;
-        }
-
-        ExcelTableColumn dataColumn;
-        private void cmbBox_columnSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (cmbBox_columnSelector.SelectedItem is ExcelTableColumn dc)
-            {                                
-                dataColumn = dc;
-                IsManipulationButtonsEnabled = true;
-            }
-            else
-            {
-                dataColumn = null;
-                IsManipulationButtonsEnabled = false;
-            }
-        }
-
-
-
-        public bool IsSheetSelectorEnabled
-        {
-            get { return (bool)GetValue(IsSheetSelectorEnabledProperty); }
-            set { SetValue(IsSheetSelectorEnabledProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for IsSheetSelectorEnabled.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsSheetSelectorEnabledProperty =
-            DependencyProperty.Register("IsSheetSelectorEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
-
-
-
-        public bool IsExportButtonEnabled
-        {
-            get { return (bool)GetValue(IsExportButtonEnabledProperty); }
-            set { SetValue(IsExportButtonEnabledProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for IsExportButtonEnabled.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsExportButtonEnabledProperty =
-            DependencyProperty.Register("IsExportButtonEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
-
-
-        public bool IsColumnSelectorEnabled
-        {
-            get { return (bool)GetValue(IsColumnSelectorEnabledProperty); }
-            set { SetValue(IsColumnSelectorEnabledProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for IsColumnSelectorEnabled.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsColumnSelectorEnabledProperty =
-            DependencyProperty.Register("IsColumnSelectorEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
-
-
-
-        public bool IsManipulationButtonsEnabled
-        {
-            get { return (bool)GetValue(IsManipulationButtonsEnabledProperty); }
-            set { SetValue(IsManipulationButtonsEnabledProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for IsManipulationButtonsEnabled.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IsManipulationButtonsEnabledProperty =
-            DependencyProperty.Register("IsManipulationButtonsEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
 
         class UplineInfo
         {
@@ -457,7 +158,9 @@ namespace Phone_Number_Normalizer
 
             stepper.Steps.Clear();
             var _custID = customerID.Text.Trim();
-             
+
+            var _theCookie = Properties.Settings.Default.HCookie;
+
             try
             {
                 client = new RestClient($"https://www.sabella.com.my/secure/orders.php?startdate=&enddate=&payment_method=&shipping_method=&status=&search_field={_custID}&platform=all&search=Search+Order")
@@ -475,7 +178,7 @@ namespace Phone_Number_Normalizer
                 request.AddHeader("Referer", $"https://www.sabella.com.my/secure/orders.php?startdate=&enddate=&payment_method=&shipping_method=&status=&search_field={_custID}&platform=all&search=Search+Order");
                 request.AddHeader("Accept-Encoding", "gzip, deflate, br");
                 request.AddHeader("Accept-Language", "en-US,en;q=0.9");
-                request.AddHeader("Cookie", "_ga=GA1.3.1550589700.1605787753; _fbp=fb.2.1606570879165.709323232; _gid=GA1.3.295114435.1606570879; sabellaAID=iav0l0l5gvu4q9dq18u6ukv2c1; tokenid=0cpo4mcfl23hlakhn0q1s14g85; newuser=Yes");
+                request.AddHeader("Cookie", _theCookie);
                 response = await client.ExecuteAsync(request);
 
                 var doc = new HtmlDocument();
@@ -496,7 +199,7 @@ namespace Phone_Number_Normalizer
                     {
                         _startingEmail = _a.LastChild.InnerText;//crucial line
 
-                        txtBox_endCustName.Text = _a.FirstChild.InnerText;
+                        txtBox_endCustName.Text = $"Name: {_a.FirstChild.InnerText}";
                     }
                 }
 
@@ -506,7 +209,7 @@ namespace Phone_Number_Normalizer
                     var _a = _price.ChildNodes.FirstOrDefault();
                     if (_a != null)
                     {
-                        txtBox_endCustPay.Text = _a.InnerText;
+                        txtBox_endCustPay.Text = $"Total: {_a.InnerText}";
                     }
                 }
 
@@ -516,7 +219,7 @@ namespace Phone_Number_Normalizer
                     var _a = _date.ChildNodes.FirstOrDefault();
                     if (_a != null)
                     {
-                        txtBox_endCustDate.Text = _a.InnerText;
+                        txtBox_endCustDate.Text = $"Order Date: {_a.InnerText}";
                     }
                 }
 
@@ -526,7 +229,7 @@ namespace Phone_Number_Normalizer
                     var _a = _status.ChildNodes.FirstOrDefault();
                     if (_a != null)
                     {
-                        txtBox_endCustStatus.Text = _a.InnerText;
+                        txtBox_endCustStatus.Text = $"Status: {_a.InnerText}";
                     }
                 }
 
@@ -742,6 +445,34 @@ namespace Phone_Number_Normalizer
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_register_cookie_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtBox_cookie.Text))
+            {
+                Properties.Settings.Default.HCookie = txtBox_cookie.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void btn_DHLTracking_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://api-eu.dhl.com/track/shipments?trackingNumber=5320118156352500&language=en&limit=5")
+            {
+                Timeout = -1
+            };
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("accept", "application/json");
+            request.AddHeader("DHL-API-Key", "n6PrQ9GcfQVXapm1MoCOhkXWeztA302m");
+            IRestResponse response = client.Execute(request);
+
+            var _shipments = DHL.DhlShipments.FromJson(response.Content);
+
+            foreach (var item in _shipments.Shipments)
+            {
+                System.Diagnostics.Debug.WriteLine($"{item.Status.StatusStatus} {item.Destination.Address.AddressLocality}");
             }
         }
     }
